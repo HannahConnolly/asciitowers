e2dc104dd67ecc548d64027dcd6ee78aebb719c4
CollinCo <connollycollin@gmail.com>
2019-07-15 14:41:10 -0700

    reverted testing column

--- src/Game.js
+++ src/Game.js
@@ -8,7 +8,7 @@ import Instructions from './Instructions';
 class Game extends React.Component {
   state = {
     board: [
-      [5, 4, 3, 2, 1],
+      [1, 2, 3, 4, 5],
       ['|', '|', '|', '|', '|'],
       ['|', '|', '|', '|', '|']
     ],
