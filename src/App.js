e957eea66c5cdb57eca21797216113bbc2c453fb
CollinCo <connollycollin@gmail.com>
2019-07-17 02:29:03 -0700

    final changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Game from './Game';
 function App() {
   return (
     <div className='App'>
-      <header className='App-header' background-color='red'>
+      <header className='App-header'>
         <h1>Towers of Hanoi</h1>
       </header>
       <Game />
