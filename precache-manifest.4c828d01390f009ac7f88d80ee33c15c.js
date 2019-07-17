f202776e366b392dfa715ac4702599dc1818c720
CollinCo <connollycollin@gmail.com>
2019-07-17 02:39:07 -0700

    Updates

---
+++ precache-manifest.4c828d01390f009ac7f88d80ee33c15c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8987e2f3418aad190d2329d10c61198",
+    "url": "/asciitowers/index.html"
+  },
+  {
+    "revision": "adaadfd5f3695dfd30af",
+    "url": "/asciitowers/static/css/main.8dd2af23.chunk.css"
+  },
+  {
+    "revision": "3686561a180e40630dea",
+    "url": "/asciitowers/static/js/2.c1c82be1.chunk.js"
+  },
+  {
+    "revision": "adaadfd5f3695dfd30af",
+    "url": "/asciitowers/static/js/main.894ce4e8.chunk.js"
+  },
+  {
+    "revision": "adbf6a8986456f6bd20c",
+    "url": "/asciitowers/static/js/runtime~main.c2e8901a.js"
+  }
+]);
